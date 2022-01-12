c84fe09c20231b234a951cfb83f51e983a654ebf
Ryan Finnie <monstermash2008@gmail.com>
2022-01-12 23:07:23 +1300

    add gradient layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      title="Ryan Finnie"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/smo1d0p4gvkukc8/10922657_943924038959586_4676463445821158801_o.jpg?dl=0"
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
